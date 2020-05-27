60db938ceeb146e3909e8f19f6f5a933f7eff7dd
harakoji05 <harakoji.0514@gmail.com>
2020-05-27 22:25:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.box1').fadeIn(function(){
+		$('.box1').css({
+    	'background-color': "#0000FF",
+    	'height': '100px',
+    	'weight': '200px'
+    	 }).fadeOut();
+    });
+});
